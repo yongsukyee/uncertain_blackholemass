844942a14663206b40b1ef74432f0bbd3b96666a
Suk Yee Yong <yongsukyee@gmail.com>
2023-04-25 23:10:27 +1000

    delete get_config script

--- lib/get_config.py
+++
@@ -1,9 +0,0 @@
-# GET CONFIGS
-
-import yaml
-
-def get_config(config_path='config/config.yaml'):
-    """Return configs"""
-    print(f"Load config file >> {config_path}")
-    with open(config_path, 'r') as f:
-        return yaml.safe_load(f)
